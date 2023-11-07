3ecf7eb3220db2665ddea824c3335b4fba341af1
MohamedGrissa0 <120946859+MohamedGrissa0@users.noreply.github.com>
2023-11-07 09:58:08 +0100

    Update emailRoutes.js

--- Routes/emailRoutes.js
+++ Routes/emailRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const { User } = require("../models/user");
+const { User } = require("../Models/User");
 const bcrypt = require("bcrypt")
 const router = express.Router();
 
