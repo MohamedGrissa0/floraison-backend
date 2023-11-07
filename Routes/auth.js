5bc8032cf1175e32dd35f888815305d83bffc8b3
MohamedGrissa0 <120946859+MohamedGrissa0@users.noreply.github.com>
2023-11-07 09:51:50 +0100

    Update auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
-const { User } = require("../models/user");
+const { User } = require("../models/User");
 const jwt = require('jsonwebtoken');
 
 
